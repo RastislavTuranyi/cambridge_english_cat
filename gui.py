2d27bf43711d6325e102cee016da97e4f973ec24
Rastislav <t.rastislavv@gmail.com>
2021-12-12 19:34:49 +0100

    Complete most questions

---
+++ gui.py
@@ -0,0 +1,279 @@
+from abc import abstractmethod
+
+import wx
+import wx.richtext as rt
+
+from tester import Tester
+
+
+class MainWindow(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, size=(700, 500),  title='Cambridge English placement test')
+        self.main_panel = wx.Panel(self)
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Create first question
+        self.tester = Tester()
+        self.tester.get_question()
+
+        self.build_panel()
+
+        self.main_panel.SetSizer(self.main_sizer)
+        self.Show()
+
+    def build_panel(self):
+        # Create sizer for the panel
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Create an instance of a panel class depending on question type
+        self.panel = panel_classes[self.tester.question.name](self.main_panel, self)
+        self.main_sizer.Add(self.panel, 0, wx.EXPAND|wx.ALL)
+
+        # Add button to submit question
+        self.submit = wx.Button(self.panel, label='Submit')
+        self.submit.Bind(wx.EVT_BUTTON, self.on_next)
+        self.Bind(wx.EVT_CHAR_HOOK, self.on_next_enter)
+        self.sizer.Add(self.submit, 0, wx.ALIGN_RIGHT)
+
+        self.panel.SetSizer(self.sizer)
+        self.main_panel.Layout()
+
+    def new_question(self):
+        # Destroy the panel containing the old question
+        for child in self.panel.GetChildren():
+            child.Destroy()
+        self.panel.Destroy()
+
+        # Build a new panel
+        self.build_panel()
+
+    def on_next(self, event):
+        self.tester.answers[self.tester.qno, 0] = self.panel.get_answer()
+        self.tester.check_answer()
+        print(self.tester.answers[:5])
+        self.tester.get_question()
+        self.new_question()
+
+    def on_next_enter(self, event):
+        if event.GetKeyCode() == wx.WXK_RETURN:
+            self.on_next(event)
+
+
+class TemplatePanel(wx.Panel):
+    def __init__(self, parent, frame):
+        super().__init__(parent)
+        self.frame = frame
+        self.question = rt.RichTextCtrl(self, size=(-1, 350), style=rt.RE_MULTILINE | rt.RE_READONLY | wx.BORDER_NONE)
+
+        self.question.Freeze()
+        self.question.BeginFont(wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+        self.question.WriteText('. '.join([str(frame.tester.qno + 1), frame.tester.question.instruction]))
+        self.question.EndFont()
+
+        self.question.Newline()
+        self.question.Newline()
+        self.question.Newline()
+        self.question.Thaw()
+
+        frame.sizer.Add(self.question, 0, wx.ALL | wx.EXPAND)
+
+    def add_image(self):
+        self.question.Freeze()
+        self.question.WriteImage(self.frame.tester.question.image, wx.BITMAP_TYPE_PNG)
+        self.question.Newline()
+        self.question.Newline()
+        self.question.Thaw()
+
+    def add_options(self):
+        self.question.Freeze()
+
+        for title, option in zip(self.frame.tester.question.titles, self.frame.tester.question.options):
+            self.question.BeginFont(wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+            self.question.WriteText(title)
+            self.question.EndFont()
+            self.question.Newline()
+            self.question.BeginFont(wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+            self.question.WriteText(option)
+            self.question.EndFont()
+            self.question.Newline()
+            self.question.Newline()
+
+        self.question.Thaw()
+
+    def add_question(self):
+        self.question.Freeze()
+        self.question.BeginFont(wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        self.question.WriteText(self.frame.tester.question.question)
+        self.question.EndFont()
+        self.question.Thaw()
+
+    def add_text(self):
+        self.question.Freeze()
+        # If there is a title, show it
+        if self.frame.tester.question.title:
+            self.question.BeginAlignment(wx.TEXT_ALIGNMENT_CENTRE)
+            self.question.BeginFont(wx.Font(16, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+            self.question.WriteText(self.frame.tester.question.title)
+            self.question.EndFont()
+            self.question.EndAlignment()
+            self.question.Newline()
+            self.question.Newline()
+
+        # If there is a subtitle, show it
+        if self.frame.tester.question.subtitle:
+            self.question.BeginAlignment(wx.TEXT_ALIGNMENT_CENTRE)
+            self.question.BeginFont(wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_ITALIC, wx.FONTWEIGHT_NORMAL))
+            self.question.WriteText(self.frame.tester.question.subtitle)
+            self.question.EndFont()
+            self.question.EndAlignment()
+            self.question.Newline()
+            self.question.Newline()
+
+        # Show the text
+        self.question.BeginAlignment(wx.TEXT_ALIGNMENT_JUSTIFIED)
+        self.question.BeginFont(wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+        first = True
+        for paragraph in self.frame.tester.question.text.split(r'\n\n'):
+            if first:
+                first = False
+            else:
+                self.question.Newline()
+                self.question.Newline()
+            self.question.WriteText(paragraph)
+        self.question.EndFont()
+        self.question.EndAlignment()
+
+        self.question.Thaw()
+
+    def add_newlines(self):
+        self.question.Freeze()
+        self.question.Newline()
+        self.question.Newline()
+        self.question.Thaw()
+
+
+class OpenAnswerPanel(TemplatePanel):
+    def __init__(self, parent, frame):
+        super().__init__(parent, frame)
+
+        self.build_question()
+        self.question.Caret.Hide()
+
+        # Add answer options
+        self.answer = wx.TextCtrl(self)
+        frame.sizer.Add(self.answer, 0, wx.ALL | wx.EXPAND)
+
+    @abstractmethod
+    def build_question(self):
+        pass
+
+    def get_answer(self):
+        return self.answer.GetValue()
+
+
+class KeyWordTransformationsPanel(OpenAnswerPanel):
+    def build_question(self):
+        self.add_text()
+        self.add_newlines()
+
+        self.question.Freeze()
+        self.question.BeginFont(wx.Font(14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+        self.question.WriteText(self.frame.tester.question.keyword)
+        self.question.EndFont()
+        self.question.Thaw()
+
+        self.add_newlines()
+        self.add_question()
+
+
+class OpenClozePanel(OpenAnswerPanel):
+    def build_question(self):
+        self.add_question()
+
+
+class WordFormationPanel(OpenAnswerPanel):
+    def build_question(self):
+        self.add_question()
+        self.add_newlines()
+        self.add_text()
+
+
+class MultiplePanel(TemplatePanel):
+    def __init__(self, parent, frame):
+        super().__init__(parent, frame)
+
+        labels = self.build_question()
+        self.question.Caret.Hide()
+
+        # Create the first radio button, so that they are a group
+        self.radiobuttons = []
+        self.radiobuttons.append(wx.RadioButton(self, label=labels[0], style=wx.RB_GROUP))
+        frame.sizer.Add(self.radiobuttons[0], 0, wx.LEFT)
+        # Create the rest of the buttons
+        for label in labels[1:]:
+            temp = wx.RadioButton(self, label=label)
+            self.radiobuttons.append(temp)
+            frame.sizer.Add(temp, 0, wx.LEFT)
+
+    @abstractmethod
+    def build_question(self):
+        pass
+
+    def get_answer(self):
+        letters = 'ABCDEFGHJKILMNOPQRSTUVWXYZ'
+        for i, button in enumerate(self.radiobuttons):
+            if button.GetValue():
+                return letters[i]
+
+
+class GappedTextPanel(MultiplePanel):
+    def build_question(self):
+        self.add_text()
+        self.add_newlines()
+        self.add_options()
+        return self.frame.tester.question.titles
+
+
+class MultipleChoiceClozePanel(MultiplePanel):
+    def build_question(self):
+        self.add_question()
+        return self.frame.tester.question.options
+
+
+class MultipleMatchPanel(MultiplePanel):
+    def build_question(self):
+        self.add_question()
+        self.add_newlines()
+        self.add_options()
+        return self.frame.tester.question.titles
+
+
+class MultipleChoicePanel(MultiplePanel):
+    def build_question(self):
+        self.add_text()
+        self.add_newlines()
+        self.add_question()
+        return self.frame.tester.question.options
+
+
+class ReadPicturePanel(MultiplePanel):
+    def build_question(self):
+        self.add_image()
+
+        if self.frame.tester.question.question:
+            self.add_newlines()
+            self.add_question()
+
+        return self.frame.tester.question.options
+
+
+panel_classes = {'open cloze': OpenClozePanel, 'multiple-choice cloze': MultipleChoiceClozePanel,
+                 'multiple choice': MultipleChoicePanel, 'multiple match': MultipleMatchPanel,
+                 'read picture': ReadPicturePanel, 'word formation': WordFormationPanel, 'gapped text': GappedTextPanel,
+                 'key word transformations': KeyWordTransformationsPanel}
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MainWindow()
+    app.MainLoop()
---
+++ tester.py
@@ -0,0 +1,347 @@
+from abc import ABC, abstractmethod
+import csv
+import os
+import random
+
+import numpy as np
+
+
+class Tester:
+
+    def __init__(self):
+        self.difficulty = temp
+        self.qno = -1
+        self.qtypes = np.zeros(30, dtype=object)
+        self.used_qs = {}
+        self.answers = np.zeros((30, 3), dtype=object)
+
+    def get_question(self):
+        qclass = self.get_question_class()
+        self.qno += 1
+        self.question = qclass(self.difficulty, self.qtypes[self.qno], self.used_qs)
+        # Save the correct answers
+        self.answers[self.qno, 1] = '/'.join(self.question.answers)
+
+        # Save which question has been given to prevent repetition
+        if self.question.name not in ['gapped text', 'multiple match']:
+            if self.used_qs.get(self.question.csv_path, None) is not None:
+                self.used_qs[self.question.csv_path].append(self.question.lineno)
+            else:
+                self.used_qs[self.question.csv_path] = [self.question.lineno]
+        else:
+            if self.used_qs.get(self.question.csv_path, None) is not None:
+                self.used_qs[self.question.csv_path].append((self.question.block, self.question.qnumber))
+            else:
+                self.used_qs[self.question.csv_path] = [(self.question.block, self.question.qnumber)]
+
+    def get_difficulty(self):
+        return None
+
+    def get_question_class(self):
+        randnum = random.randint(0, len(self.difficulty)-1)
+        keys = list(self.difficulty.keys())
+
+        while keys[randnum] == self.qtypes[self.qno]:
+            randnum = random.randint(0, len(self.difficulty) - 1)
+
+        self.qtypes[self.qno+1] = keys[randnum]
+        return self.difficulty[keys[randnum]]
+
+    def check_answer(self):
+        self.answers[self.qno, 2] = self.question.check_answer(self.answers[self.qno, 0])
+
+
+class Question(ABC):
+    def __init__(self, difficulty, qtype, used_qs):
+        self.lineno = 0
+
+        self.answers = []
+        self.image = ''
+        self.options = []
+        self.question = ''
+        self.text = ''
+        self.title = ''
+        self.titles = []
+        self.subtitle = ''
+
+        pwd = os.path.dirname(os.path.realpath(__file__))
+        self.csv_path = os.path.join(pwd, 'Data', difficulty.name, ''.join([qtype, '.csv']))
+
+        with open(self.csv_path, encoding='utf-8') as file:
+            csv_file = [i for i in csv.reader(file, delimiter=';') if i]
+            file_length = len(csv_file)
+
+        self.select_question(csv_file, file_length, used_qs)
+
+    @abstractmethod
+    def select_question(self, csv_file, file_length, used_qs):
+        pass
+
+    @abstractmethod
+    def check_answer(self, submitted_answer):
+        pass
+
+
+class LineQuestion(Question, ABC):
+    def select_question(self, csv_file, file_length, used_qs):
+        for __ in range(file_length * 5):
+            self.lineno = random.randint(0, file_length - 1)
+            if self.lineno not in used_qs.get(self.csv_path, []):
+                break
+
+    @abstractmethod
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class BlockQuestion(Question, ABC):
+    block_length = 0
+    question_location = 0
+
+    def select_question(self, csv_file, file_length, used_qs):
+        if file_length % self.block_length != 0:
+            raise Exception(file_length)
+        else:
+            blocks = int(file_length / self.block_length)
+
+        for __ in range(blocks*5):
+            self.block = random.randint(0, blocks - 1)
+            self.questions = csv_file[self.block * self.block_length + self.question_location]
+            for ___ in range(len(self.questions)*5):
+                self.qnumber = random.randint(0, len(self.questions) - 1)
+                if (self.block, self.qnumber) not in used_qs.get(self.csv_path, []):
+                    return
+
+    @abstractmethod
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class MultipleChoiceQuestion(Question, ABC):
+    @abstractmethod
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+    def check_answer(self, submitted_answer):
+        if submitted_answer == self.answers[0]:
+            return 1
+        else:
+            return 0
+
+
+class OpenAnswerQuestion(Question, ABC):
+    @abstractmethod
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+    def check_answer(self, submitted_answer):
+        for correct_answer in self.answers:
+            if submitted_answer.strip().lower() == correct_answer.strip().lower():
+                return 1
+        else:
+            return 0
+
+
+class GappedText(MultipleChoiceQuestion, BlockQuestion):
+    name = 'gapped text'
+    instruction = 'Several sentences/paragraphs have been removed from the text below. Choose the option that has been' \
+                  ' removed from gap number '
+    block_length = 3
+    question_location = 1
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+        self.title = csv_file[self.block * self.block_length][0]
+        self.subtitle = csv_file[self.block * self.block_length][1]
+        self.text = csv_file[self.block * self.block_length][2]
+
+        self.answers = list(csv_file[self.block * self.block_length + 1][self.qnumber])
+        self.options = csv_file[self.block * self.block_length + 2]
+
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        self.titles = [letters[i] for i, __ in enumerate(csv_file[self.block * self.block_length + 2])]
+
+        # Add the number of the gap that should be filled into instructions
+        self.instruction = ''.join([self.instruction, str(self.qnumber + 1)])
+
+        # Replace the gaps that have been answered previously with the correct answer
+        for (block, qnumber) in used_qs.get(self.csv_path, []):
+            correct_answer = letters.find(csv_file[block * self.block_length + 1][qnumber])
+            answer_text = self.options[correct_answer]
+            self.text = self.text.replace(''.join(['(', str(correct_answer), ') ..........']), answer_text)
+
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class KeyWordTransformations(LineQuestion):
+    name = 'key word transformations'
+    instruction = ''
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+        line = csv_file[self.lineno]
+        self.text = line[0]
+        self.keyword = line[1]
+        self.question = line[2]
+        self.answers = line[3:]
+
+    def check_answer(self, submitted_answer):
+        scores = np.zeros(len(self.answers))
+        submitted_words = submitted_answer.replace(',', '').replace('.', '').replace(';', '').split()
+
+        # Score the submitted answer against each possibple correct answer
+        for answer_number, correct_answer in enumerate(self.answers):
+            halves = correct_answer.split('|')
+            offset = [0]
+
+            # Score the two halves (separated by |) separately
+            for half_number, half in enumerate(halves):
+                words = half.split()
+
+                # For the second half, search where it may be beginning by searching for the first word
+                if half_number == 1:
+                    offset = [j for j, answer in enumerate(submitted_answer) if answer == words[0]]
+                    if not offset:
+                        break
+
+                # Go through all the occurrences of the first word of the second half, and increase the score only if
+                # all the words after that also match
+                for off in offset:
+                    for i, word in enumerate(words):
+                        try:
+                            if word != submitted_words[i + off]:
+                                break
+                        except IndexError:
+                            break
+                    else:
+                        scores[answer_number] += 1
+                        break
+
+
+class MultipleChoice(LineQuestion, MultipleChoiceQuestion):
+    name = 'multiple choice'
+    instruction = 'Read the text below, then answer the question about the text.'
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+        line = csv_file[self.lineno]
+        self.title = line[0]
+        self.subtitle = line[1]
+        self.text = line[2]
+        self.answers = list(line[3])
+        self.question = line[4]
+        self.options = line[5:]
+
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class MultipleMatch(MultipleChoiceQuestion, BlockQuestion):
+    name = 'multiple match'
+    block_length = 4
+    question_location = 1
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+        self.instruction = csv_file[self.block*4][0]
+        self.question = self.questions[self.qnumber]
+        self.answers = list(csv_file[self.block*4+3][self.qnumber])
+        self.options = csv_file[self.block*4+2][1::2]
+
+        if csv_file[self.block*4+2][0]:
+            self.titles = csv_file[self.block*4+2][::2]
+        else:
+            letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            self.titles = [letters[i] for i, __ in enumerate(csv_file[self.block*4+2][::2])]
+
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class MultipleChoiceCloze(LineQuestion, MultipleChoiceQuestion):
+    name = 'multiple-choice cloze'
+    instruction = 'Select which word fits best into the gap. There is exactly 1 correct answer.'
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+        line = csv_file[self.lineno]
+        self.question = line[0]
+        self.answers = list(line[1])
+        self.options = line[2:]
+
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class OpenCloze(LineQuestion, OpenAnswerQuestion):
+    name = 'open cloze'
+    instruction = 'Write the missing word (only 1) into the field below. Take care with spelling. There' \
+                  ' may be multiple correct answers, but only enter one.'
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+
+        line = csv_file[self.lineno]
+        self.question = line[0]
+        self.answers = line[1:]
+
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class ReadPicture(LineQuestion, MultipleChoiceQuestion):
+    name = 'read picture'
+    instruction = 'For the question below, choose the correct answer.'
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+        line = csv_file[self.lineno]
+
+        self.answers = list(line[0])
+        self.question = line[1]
+        self.options = line[2:]
+        self.image = os.path.join(os.path.split(self.csv_path)[0], ''.join([self.name, str(self.lineno), '.png']))
+
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class WordFormation(LineQuestion, OpenAnswerQuestion):
+    name = 'word formation'
+    instruction = 'Modify the given stem word so that it fits into the gap in the sentence.'
+
+    def select_question(self, csv_file, file_length, used_qs):
+        super().select_question(csv_file, file_length, used_qs)
+        line = csv_file[self.lineno]
+
+        self.question = line[0]
+        self.title = line[1]
+        self.answers = line[2:]
+
+    def check_answer(self, submitted_answer):
+        return super().check_answer(submitted_answer)
+
+
+class NamedDict(dict):
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
+        super().__init__(*args, **kwargs)
+
+
+temp = NamedDict('B2', [('word formation', WordFormation), ('key word transformations', KeyWordTransformations)])
+A2 = NamedDict('A2', [('multiple choice', MultipleChoice), ('multiple match', MultipleMatch),
+                      ('multiple-choice cloze', MultipleChoiceCloze), ('open cloze', OpenCloze),
+                      ('read picture', ReadPicture)])
+B1 = NamedDict('B1', [('gapped text', GappedText), ('multiple choice', MultipleChoice), ('multiple match', MultipleMatch),
+                      ('multiple-choice cloze', MultipleChoiceCloze), ('open cloze', OpenCloze),
+                      ('read picture', ReadPicture)])
+B2 = NamedDict('B2', [('gapped text', GappedText), ('key word transformations', KeyWordTransformations),
+                      ('multiple choice', MultipleChoice), ('multiple match', MultipleMatch),
+                      ('multiple-choice cloze', MultipleChoiceCloze), ('open cloze', OpenCloze),
+                      ('word formation', WordFormation)])
