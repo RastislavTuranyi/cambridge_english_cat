26052aae15e8e0a9b4146b710f2599c541f63a03
Rastislav <t.rastislavv@gmail.com>
2021-12-24 11:19:04 +0100

    Add exam review screen and make sizer a panel attribute

--- gui.py
+++ gui.py
@@ -9,9 +9,11 @@ from tester import Tester, TestEndError, LowCertaintyError, InconsistentResultsE
 class MainWindow(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, size=(1000, 600),  title='Cambridge English placement test')
+        self.panels = []
+        self.old_sizers = []
+
         self.main_panel = wx.Panel(self)
         self.main_sizer = wx.GridSizer(cols=1, rows=1, vgap=5, hgap=5)
-        # TODO: play with boxsizers and gridsizers to perfect the layout
         # Create first question
         self.tester = Tester()
         self.tester.get_question()
@@ -24,27 +26,14 @@ class MainWindow(wx.Frame):
         self.Show()
 
     def build_panel(self):
-        # Create sizer for the panel
-        self.sizer = wx.FlexGridSizer(cols=1, rows=2, vgap=5, hgap=5)
-
-        print(self.tester.difficulty.name, self.tester.qno % 5, self.tester.scores)
-
-        if isinstance(self.tester.question, MultipleChoiceQuestion):
-            self.button_sizer = wx.FlexGridSizer(cols=3, rows=4, vgap=5, hgap=5)
-            self.button_sizer.AddGrowableCol(0, 1)
-            self.button_sizer.AddGrowableCol(1, 1)
-            self.button_sizer.AddGrowableCol(2, 1)
-        else:
-            self.button_sizer = wx.BoxSizer(wx.VERTICAL)
-
         # Create an instance of a panel class depending on question type
         self.panel = panel_classes[self.tester.question.name](self.main_panel, self)
 
         self.main_sizer.Add(self.panel, 1, wx.EXPAND|wx.ALL)
 
         # Add button to submit question
-        self.submit = wx.Button(self.panel, label='Submit')
-        self.submit.Bind(wx.EVT_BUTTON, self.on_next)
+        self.panel.submit = wx.Button(self.panel, label='Submit')
+        self.panel.submit.Bind(wx.EVT_BUTTON, self.on_next)
 
         # Allow ENTER key to be able to be used to submit questions
         if self.panel.hook == 'multiple':
@@ -52,18 +41,18 @@ class MainWindow(wx.Frame):
         elif self.panel.hook == 'open':
             self.Bind(wx.EVT_TEXT_ENTER, self.on_next)
 
-        self.button_sizer.Add(self.submit, 0, wx.ALIGN_RIGHT)
-        self.sizer.Add(self.button_sizer, 1, wx.EXPAND)
+        self.panel.button_sizer.Add(self.panel.submit, 0, wx.ALIGN_RIGHT)
+        self.panel.sizer.Add(self.panel.button_sizer, 1, wx.EXPAND)
 
-        self.sizer.AddGrowableCol(0, 1)
-        self.sizer.AddGrowableRow(0, 1)
+        self.panel.sizer.AddGrowableCol(0, 1)
+        self.panel.sizer.AddGrowableRow(0, 1)
 
-        self.panel.SetSizer(self.sizer)
+        self.panel.SetSizer(self.panel.sizer)
         self.panel.Layout()
         self.main_panel.Layout()
 
     def new_question(self):
-        self.destroy_panel()
+        self.hide_panel()
 
         if self.panel.hook == 'multiple':
             self.Unbind(wx.EVT_CHAR_HOOK)
@@ -83,10 +72,22 @@ class MainWindow(wx.Frame):
         except RuntimeError:  # Ignore if the panel has been destroyed already
             pass
 
+    def hide_panel(self):
+        self.panel.submit.Unbind(wx.EVT_BUTTON)
+
+        try:
+            self.panel.answer.Destroy()
+        except AttributeError:
+            for rb in self.panel.radiobuttons:
+                rb.Destroy()
+
+        self.panels.append(self.panel)
+        self.main_sizer.Remove(0)
+        self.panel.Hide()
+
     def on_next(self, event):
         self.tester.submitted_answers.append(self.panel.get_answer())
         self.tester.check_answer()
-        #print(self.tester.submitted_answers[:5])
         # Normally, get new question and display it
         try:
             self.tester.get_question()
@@ -118,11 +119,30 @@ class MainWindow(wx.Frame):
         self.new_question()
 
     def on_show_results(self, event):
-        self.destroy_panel()
+        self.hide_panel()
 
         self.sizer = wx.FlexGridSizer(cols=1, rows=2, vgap=5, hgap=5)
         self.panel = ResultsPanel(self.main_panel, self)
         self.main_sizer.Add(self.panel, 0, wx.EXPAND | wx.ALL)
+
+        self.button = wx.Button(self.panel, label='Review answers')
+        self.button.Bind(wx.EVT_BUTTON, self.on_review_results)
+        self.sizer.Add(self.button, 0, wx.ALIGN_RIGHT)
+
+        self.panel.SetSizerAndFit(self.sizer)
+        self.panel.Layout()
+        self.main_panel.Layout()
+
+    def on_review_results(self, event):
+        self.destroy_panel()
+
+        self.sizer = wx.FlexGridSizer(cols=5, rows=(len(self.panels)+1), vgap=15, hgap=15)
+        self.panel = ReviewPanel(self.main_panel, self)
+
+        self.main_sizer.Add(self.panel, 1, wx.EXPAND)
+
+        for i in range(5):
+            self.sizer.AddGrowableCol(i, 1)
 
         self.panel.SetSizerAndFit(self.sizer)
         self.panel.Layout()
@@ -135,13 +155,25 @@ class TemplatePanel(wx.Panel):
     def __init__(self, parent, frame):
         super().__init__(parent)
         self.frame = frame
+
+        # Create sizer for the panel
+        self.sizer = wx.FlexGridSizer(cols=1, rows=2, vgap=5, hgap=5)
+
+        if isinstance(frame.tester.question, MultipleChoiceQuestion):
+            self.button_sizer = wx.FlexGridSizer(cols=3, rows=4, vgap=5, hgap=5)
+            self.button_sizer.AddGrowableCol(0, 1)
+            self.button_sizer.AddGrowableCol(1, 1)
+            self.button_sizer.AddGrowableCol(2, 1)
+        else:
+            self.button_sizer = wx.BoxSizer(wx.VERTICAL)
+
         self.question = rt.RichTextCtrl(self, style=rt.RE_MULTILINE | rt.RE_READONLY | wx.BORDER_NONE)
 
         self.question.Freeze()
         self.add_instruction()
         self.question.Thaw()
 
-        frame.sizer.Add(self.question, 1, wx.ALL | wx.EXPAND)
+        self.sizer.Add(self.question, 1, wx.ALL | wx.EXPAND)
 
     def add_instruction(self):
         """
@@ -259,7 +291,7 @@ class OpenAnswerPanel(TemplatePanel):
 
         # Add answer options
         self.answer = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
-        frame.button_sizer.Add(self.answer, 0, wx.ALL | wx.EXPAND)
+        self.button_sizer.Add(self.answer, 0, wx.ALL | wx.EXPAND)
 
     @abstractmethod
     def build_question(self):
@@ -345,31 +377,31 @@ class MultiplePanel(TemplatePanel):
         self.radiobuttons = []
         self.radiobuttons.append(wx.RadioButton(self, label=labels[0], style=wx.RB_GROUP))
         self.radiobuttons[0].SetFont(font)
-        frame.button_sizer.Add(self.radiobuttons[0], 0, wx.LEFT)
+        self.button_sizer.Add(self.radiobuttons[0], 0, wx.LEFT)
 
         # Create the rest of the buttons
         if len(labels) <= 4:
             for i, label in enumerate(labels[1:]):
                 # Fill up cells with empty text so that the options appear on left below one another
-                frame.button_sizer.Add(wx.StaticText(self), 0)
-                frame.button_sizer.Add(wx.StaticText(self), 0)
+                self.button_sizer.Add(wx.StaticText(self), 0)
+                self.button_sizer.Add(wx.StaticText(self), 0)
 
                 temp = wx.RadioButton(self, label=label)
                 temp.SetFont(font)
                 self.radiobuttons.append(temp)
-                frame.button_sizer.Add(temp, 0, wx.LEFT)
+                self.button_sizer.Add(temp, 0, wx.LEFT)
 
-            frame.button_sizer.Add(wx.StaticText(self), 0)  # To make the button appear on the left
+            self.button_sizer.Add(wx.StaticText(self), 0)  # To make the button appear on the left
         else:
             for label in labels[1:]:
                 temp = wx.RadioButton(self, label=label)
                 temp.SetFont(font)
                 self.radiobuttons.append(temp)
-                frame.button_sizer.Add(temp, 0, wx.LEFT)
+                self.button_sizer.Add(temp, 0, wx.LEFT)
 
             if len(labels) < 8:
                 for i in range(8 - len(labels)):
-                    frame.button_sizer.Add(wx.StaticText(self), 0)
+                    self.button_sizer.Add(wx.StaticText(self), 0)
 
     @abstractmethod
     def build_question(self):
@@ -536,18 +568,77 @@ class ResultsPanel(wx.Panel):
         frame.sizer.AddGrowableCol(0, 1)
         frame.sizer.AddGrowableRow(0, 1)
 
-    def write_result(self, result):
-        self.text.BeginFont(wx.Font(22, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_EXTRABOLD))
-        self.text.WriteText(result)
-        self.text.EndFont()
-        self.text.Newline()
+
+class ReviewPanel(wx.Panel):
+    def __init__(self, parent, frame):
+        super().__init__(parent)
+        self.panels = frame.panels
+        self.main_sizer = frame.main_sizer
+        # TODO: think about wrapping the correct answers
+        self.SetBackgroundColour(wx.Colour(255, 255, 255))
+
+        header_font = wx.Font(12, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_SEMIBOLD)
+        header1 = wx.StaticText(self, label='Question number')
+        header1.SetFont(header_font)
+        frame.sizer.Add(header1, 0, wx.ALIGN_CENTER)
+
+        header2 = wx.StaticText(self, label='Difficulty')
+        header2.SetFont(header_font)
+        frame.sizer.Add(header2, 0, wx.ALIGN_CENTER)
+
+        header3 = wx.StaticText(self, label='Points obtained')
+        header3.SetFont(header_font)
+        frame.sizer.Add(header3, 0, wx.ALIGN_CENTER)
+
+        header4 = wx.StaticText(self, label='Your answer')
+        header4.SetFont(header_font)
+        frame.sizer.Add(header4, 0, wx.ALIGN_CENTER)
+
+        header5 = wx.StaticText(self, label='Correct answer')
+        header5.SetFont(header_font)
+        frame.sizer.Add(header5, 0, wx.ALIGN_CENTER)
+
+        buttons = []
+        levels = ['pre-A1', 'A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+        for index, panel in enumerate(frame.panels):
+            buttons.append(wx.Button(self, label=str(index + 1)))
+            buttons[index].Bind(wx.EVT_BUTTON, self.on_button)
+            buttons[index].index = index
+            frame.sizer.Add(buttons[index], 0, wx.ALIGN_CENTER)
+
+            frame.sizer.Add(wx.StaticText(self, label=levels[frame.tester.difficulties[index]]), 0, wx.ALIGN_CENTER)
+            frame.sizer.Add(wx.StaticText(self, label=str(frame.tester.scores[index])), 0, wx.ALIGN_CENTER)
+            frame.sizer.Add(wx.StaticText(self, label=frame.tester.submitted_answers[index]), 0)
+            frame.sizer.Add(wx.StaticText(self, label=frame.tester.correct_answers[index]), 0)
+
+    def on_button(self, event):
+        self.temp_panel = self.panels[event.GetEventObject().index]
+
+        self.temp_panel.submit.Bind(wx.EVT_BUTTON, self.on_return)
+        self.temp_panel.submit.SetLabel('Back')
+
+        self.main_sizer.Remove(0)
+        self.main_sizer.Add(self.temp_panel, 1, wx.EXPAND)
+
+        self.temp_panel.SetSizerAndFit(self.temp_panel.sizer)
+        self.main_sizer.Layout()
+
+        self.temp_panel.Show()
+        self.Hide()
+
+    def on_return(self, event):
+        self.main_sizer.Remove(0)
+        self.main_sizer.Add(self, 1, wx.EXPAND)
+
+        self.temp_panel.Hide()
+        self.Show()
 
 
 if __name__ == '__main__':
     import wx.lib.inspection
 
     app = wx.App()
-    # wx.lib.inspection.InspectionTool().Show()
+    #wx.lib.inspection.InspectionTool().Show()
 
     frame = MainWindow()
     app.MainLoop()
